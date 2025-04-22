f3b19055424bf2c2d8872d0d723d21a1f6aa446b
nicolasnocete <nicolasnocete@gmail.com>
2025-04-22 14:54:56 -0300

    fix: correct 'platforms' and 'player' references to 'this.platforms' and 'this.player' in create method

--- scenes/Game.js
+++ scenes/Game.js
@@ -32,16 +32,16 @@ export default class Game extends Phaser.Scene {
 
     this.platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
-    platforms.create(600, 400, "ground");
-    platforms.create(50, 250, "ground");
-    platforms.create(750, 220, "ground");
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
 
     this.player = this.physics.add.sprite(100, 450, "dude");
 
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: "left",
@@ -63,12 +63,29 @@ export default class Game extends Phaser.Scene {
       repeat: -1,
     });
 
-    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(this.player, this.platforms);
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
     // update game objects
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
