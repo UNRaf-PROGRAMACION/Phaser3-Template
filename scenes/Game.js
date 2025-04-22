dfdaabb12abcf809495bcef89193ea7d8c6a8c74
nicolasnocete <nicolasnocete@gmail.com>
2025-04-22 14:39:49 -0300

    add static platforms in create method

--- scenes/Game.js
+++ scenes/Game.js
@@ -29,6 +29,14 @@ export default class Game extends Phaser.Scene {
   create() {
     // create game objects
     this.add.image(400, 300, "sky");
+
+    let platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
   }
 
   update() {
