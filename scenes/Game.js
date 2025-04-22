b5e5d17fefb06a3188859c8b7d8698f7ad220a5f
nicolasnocete <nicolasnocete@gmail.com>
2025-04-22 14:35:28 -0300

    load assets in preload method

--- scenes/Game.js
+++ scenes/Game.js
@@ -16,6 +16,14 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     // load assets
+    this.load.image("sky", "./public/assets/sky.png");
+    this.load.image("ground", "./public/assets/platform.png");
+    this.load.image("star", "./public/assets/star.png");
+    this.load.image("bomb", "./public/assets/bomb.png");
+    this.load.spritesheet("dude", "./public/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
   }
 
   create() {
